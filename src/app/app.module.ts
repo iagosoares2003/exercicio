5c7e4bee9d24ea54d1fa50039293593973198f3d
iago soares <iago.soares2425@gmail.com>
2019-02-20 13:11:31 -0300

    enviando para o apk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { IntroPage } from '../pages/intro/intro';
+import { VideosPage } from '../pages/videos/videos';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    IntroPage,
+    VideosPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +27,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    IntroPage,
+    VideosPage
   ],
   providers: [
     StatusBar,
